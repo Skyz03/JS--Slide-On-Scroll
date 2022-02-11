11995d9f439c294fe44600307e8b1e550cf0ea0c
Skyz03 <aakibdiscoo@gmail.com>
2022-02-11 16:29:28 +0545

    Updates in the challanege

--- index.js
+++ index.js
@@ -17,8 +17,24 @@ const sliderImages = document.querySelectorAll(".slide-in");
 
 function checkSlide(e) {
   sliderImages.forEach(sliderImage => {
+
+    // half way through the image
     const slideInAt = (window.scrollY + window.innerHeight) - sliderImage.height / 2;
-    console.log(slideInAt);
+
+    // bottom of the image
+    const imageBottom = sliderImage.offsetTop + sliderImage.height;
+
+    const isHalfShown = slideInAt > sliderImage.offsetTop;
+    console.log(isHalfShown);
+
+    const isNotScrolledPAst = window.scrollY < imageBottom;
+    console.log(isNotScrolledPAst);
+
+    if (isHalfShown && isNotScrolledPAst) {
+      sliderImage.classList.add("active");
+    } else {
+      sliderImage.classList.remove("active");
+    }
   })
 }
 
